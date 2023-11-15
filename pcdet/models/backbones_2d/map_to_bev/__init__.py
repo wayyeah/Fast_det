331777f3b3df37223a2e2a2e682a23b9fdda4b0e
Way <920258461@qq.com>
2023-11-15 22:36:25 +0800

    pillar

--- pcdet/models/backbones_2d/map_to_bev/__init__.py
+++ pcdet/models/backbones_2d/map_to_bev/__init__.py
@@ -8,7 +8,7 @@ from .bev_convResH import BEVConvResH
 from .bev_convRes_TS import BEVConvResTS
 from .bev_convI import BEVConvI
 from .bev_convDepth import BEVConvDepth 
-from .bev_convWise import BEVConvWise,BEVConvWiseV2,BEVConvWiseV3,BEVConvWiseWithI,BEVConvWiseWithIV2
+from .bev_convWise import BEVConvWise,BEVConvWiseV2,BEVConvWiseV3,BEVConvWiseWithI,BEVConvWiseWithIV2,BEVConvWiseWithIV3,BEVConvWisePillar
 
 __all__ = {
     'HeightCompression': HeightCompression,
@@ -27,4 +27,6 @@ __all__ = {
     'BEVConvWiseV3':BEVConvWiseV3,
     'BEVConvWiseWithI':BEVConvWiseWithI,
     'BEVConvWiseWithIV2':BEVConvWiseWithIV2,
+    'BEVConvWiseWithIV3':BEVConvWiseWithIV3,
+    'BEVConvWisePillar':BEVConvWisePillar
 }
