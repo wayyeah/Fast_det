7c847b14fbe85d160e3decfa3cda67ce9600d0da
Way <920258461@qq.com>
2023-11-24 10:42:07 +0800

    BEVbase

--- pcdet/models/backbones_2d/map_to_bev/__init__.py
+++ pcdet/models/backbones_2d/map_to_bev/__init__.py
@@ -8,7 +8,7 @@ from .bev_convResH import BEVConvResH
 from .bev_convRes_TS import BEVConvResTS
 from .bev_convI import BEVConvI,BEVConvIV5
 from .bev_convDepth import BEVConvDepth 
-from .bev_convWise import BEVConvWise,BEVConvWiseV2,BEVConvWiseV3,BEVConvWiseWithI,BEVConvWiseWithIV2,BEVConvWiseWithIV3,BEVConvWiseWithIV4,BEVConvWiseWithIV5,BEVConvWiseWithIV6,BEVConvWiseWithIV7,BEVConvWiseWithIV8
+from .bev_convWise import BEVConvWise,BEVConvWiseV2,BEVConvWiseV3,BEVConvWiseWithI,BEVConvWiseWithIV2,BEVConvWiseWithIV3,BEVConvWiseWithIV4,BEVConvWiseWithIV5,BEVConvWiseWithIV6,BEVConvWiseWithIV7,BEVConvWiseWithIV8,BEVBase
 from .bev_spconv import BEVSPConv
 
 __all__ = {
@@ -36,4 +36,5 @@ __all__ = {
     'BEVConvWiseWithIV8':BEVConvWiseWithIV8,
     'BEVConvIV5':BEVConvIV5,
     'BEVSPConv': BEVSPConv,
+    'BEVBase':BEVBase
 }
