3f9b3b514d75262621429dfd6f087629f1b70911
Way <920258461@qq.com>
2023-11-27 09:50:45 +0800

    V5

--- pcdet/models/backbones_2d/map_to_bev/__init__.py
+++ pcdet/models/backbones_2d/map_to_bev/__init__.py
@@ -10,7 +10,7 @@ from .bev_convI import BEVConvI,BEVConvIV5
 from .bev_convDepth import BEVConvDepth 
 from .bev_convWise import BEVConvWise,BEVConvWiseV2,BEVConvWiseV3,BEVConvWiseWithI,BEVConvWiseWithIV2,BEVConvWiseWithIV3,BEVConvWiseWithIV4,BEVConvWiseWithIV5,BEVConvWiseWithIV6,BEVConvWiseWithIV7,BEVConvWiseWithIV8,BEVBase
 from .bev_spconv import BEVSPConv,BEVSPConvV2
-from .bev_convS import BEVConvS,BEVConvSV2,BEVConvSV3,BEVConvSV4
+from .bev_convS import BEVConvS,BEVConvSV2,BEVConvSV3,BEVConvSV4,BEVConvSV5
 __all__ = {
     'HeightCompression': HeightCompression,
     'PointPillarScatter': PointPillarScatter,
@@ -41,4 +41,5 @@ __all__ = {
     'BEVConvSV3':BEVConvSV3,
     'BEVConvSV4':BEVConvSV4,
     'BEVSPConvV2':BEVSPConvV2,
+    'BEVConvSV5':BEVConvSV5,
 }
