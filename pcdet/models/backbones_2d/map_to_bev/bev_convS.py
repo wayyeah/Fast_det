717b42b2522bc2535104f6dd21d5242864c77207
way <920258461@qq.com>
2023-12-05 15:14:56 +0800

    ConvsNoraml

--- pcdet/models/backbones_2d/map_to_bev/bev_convS.py
+++ pcdet/models/backbones_2d/map_to_bev/bev_convS.py
@@ -147,6 +147,31 @@ def points_to_bevs(points, point_range, batch_size,size):
     bev_i[batch_indices, 0, y_indices, x_indices] = torch.maximum(bev_i[batch_indices, 0, y_indices, x_indices], i_vals)
     return bev,bev_average_z,bev_zmin,bev_zmax,bev_i
 
+def points_to_bevs_two(points, point_range, batch_size,size):
+    x_scale_factor = size[0]/ (point_range[3] - point_range[0])
+    y_scale_factor = size[1]/ (point_range[4] - point_range[1])
+    bev_shape = (batch_size, 1, size[1] , size[0] )
+    bev = torch.ones(bev_shape, dtype=torch.float32, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))*-10
+    bev_intensity= torch.zeros(bev_shape, dtype=torch.float32, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    if(point_range[0]==0):
+        x_pixel_values = ((points[:, 1] ) * x_scale_factor).to(torch.int)
+    else:    
+        x_pixel_values = ((points[:, 1] + (point_range[3] - point_range[0]) / 2) * x_scale_factor).to(torch.int)
+    y_pixel_values = ((points[:, 2] + (point_range[4] - point_range[1]) / 2) * y_scale_factor).to(torch.int)
+    z_values = points[:, 3]
+    i_values=points[:, 4]
+    mask = (x_pixel_values < size[0]) & (x_pixel_values >= 0) & (y_pixel_values < size[1]) & (y_pixel_values >= 0)
+    batch_indices = points[mask, 0].long()
+    x_indices = x_pixel_values[mask]
+    y_indices = y_pixel_values[mask]
+    #z_vals = z_values_normalized[mask]
+    z_vals=z_values[mask] #取消z正则化 
+    i_vals=i_values[mask]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    bev[batch_indices, 0, y_indices, x_indices] = torch.maximum(bev[batch_indices, 0, y_indices, x_indices], z_vals)
+    bev_intensity[batch_indices, 0, y_indices, x_indices] = torch.maximum(bev_intensity[batch_indices, 0, y_indices, x_indices], i_vals)
+    return torch.cat([bev,bev_intensity], dim=1)  
+
+
 class BEVConvS(nn.Module):
     def __init__(self, model_cfg, **kwargs):
         super().__init__()
@@ -192,7 +217,48 @@ class BEVConvS(nn.Module):
         spatial_features = self.conv_layers(bev_combined)
         batch_dict['spatial_features'] = (spatial_features)
         return batch_dict
-    
+class BEVConvSNormal(nn.Module):
+    def __init__(self, model_cfg, **kwargs):
+        super().__init__()
+        self.model_cfg = model_cfg
+        self.num_bev_features = self.model_cfg.NUM_BEV_FEATURES
+        self.point_range=self.model_cfg.POINT_CLOUD_RANGE
+        self.size=self.model_cfg.SIZE
+        self.conv_layers = nn.Sequential(
+            # Existing layers
+            nn.Conv2d(2, 8, kernel_size=3, stride=1, padding=1), #b*8*1600*1408
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),#b*8*800*704
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  #b*16*400*352
+            # Depthwise separable convolution
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            # Final layers
+            nn.Conv2d(32, self.num_bev_features, kernel_size=3, stride=1, padding=1), #b*n*400*352
+            nn.BatchNorm2d(self.num_bev_features),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), #b*n*200*176
+        )
+    def forward(self, batch_dict):
+        """
+        Args:
+            batch_dict:
+                encoded_spconv_tensor: sparse tensor
+        Returns:
+            batch_dict:
+                spatial_features:
+
+        """
+        bev_combined=points_to_bevs_two(batch_dict['points'],self.point_range,batch_dict['batch_size'],self.size)
+        batch_dict['bev'] = bev_combined
+        spatial_features = self.conv_layers(bev_combined)
+        batch_dict['spatial_features'] = (spatial_features)
+        return batch_dict    
 class BEVConvSV2(nn.Module):
     def __init__(self, model_cfg, **kwargs):
         super().__init__()
